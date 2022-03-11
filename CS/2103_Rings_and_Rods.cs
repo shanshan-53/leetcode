4a27fa04c7514f81c78111b160b786f611d00354
楊育姍 <shanshan@yangyushandeMacBook-Air.local>
2022-03-11 18:21:11 +0800

    2103_Easy_C3

--- CS/2103_Rings_and_Rods.cs
+++ CS/2103_Rings_and_Rods.cs
@@ -1,5 +1,23 @@
 public class Solution {
     public int CountPoints(string rings) {
+        Dictionary<char, HashSet<char>> dic = new Dictionary<char, HashSet<char>>();
+        
+        for(int i = 0; i < rings.Length; i+=2){
+            if(!dic.ContainsKey(rings[i+1])){
+                dic.Add(rings[i+1], new HashSet<char>());
+            }
+            dic[rings[i+1]].Add(rings[i]);
+        }
+        
+        int result = 0;
+        foreach(var i in dic){
+            if(i.Value.Count == 3){
+                result++;
+            }
+        }
+        
+        return result;
+        /*
         bool[,] f = new bool[10,3];
         int[] t = new int[10];
         for(int i = 0; i < rings.Length; i+=2){
@@ -24,5 +42,6 @@ public class Solution {
                 result++;
         }
         return result;
+        */
     }
 }
